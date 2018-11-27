2d95d91d1b4b8fbbc230bb9734cf005eb89c7317
Flarne <ulrik.bergstrom@gmail.com>
2018-11-27 14:00:11 +0100

    Adding Jump script

--- Unity/TheTwilightAdventuresInUpAndDown/Assets/Scripts/Jump.cs
+++ Unity/TheTwilightAdventuresInUpAndDown/Assets/Scripts/Jump.cs
@@ -2,15 +2,64 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Jump : MonoBehaviour {
+public class Jump : MonoBehaviour
+{
+	public float jumpMaxTime;
+	public float jumpSpeed = 1;
+	float jumpTime;
+	float jumpMaxTimer;
+	public bool isJumping;
+
+	Rigidbody2D rb2D;
 
 	// Use this for initialization
-	void Start () {
-		
+	void Start ()
+	{
+		rb2D = GetComponent<Rigidbody2D>();
+		rb2D.gravityScale = 3;
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void Update ()
+	{
+		if (Input.GetButtonDown("Jump") && !isJumping)
+		{
+			isJumping = true;
+			jumpMaxTimer = Time.time + jumpMaxTime;
+			jumpTime = 0;
+		}
+
+		if (Input.GetButton("Jump") && Time.time < jumpMaxTimer && isJumping)
+		{
+			jumpTime += Time.deltaTime;
+
+			rb2D.velocity = new Vector2(rb2D.velocity.x, jumpSpeed);
+
+			if (jumpTime < 0.45f)
+			{
+				if (jumpSpeed < 8)
+				{
+					jumpSpeed *= 1.2f;
+				}
+			}
+			else
+			{
+				jumpSpeed *= 0.8f;
+			}
+			Debug.Log("Hoppa");
+		}
+
+		if (Input.GetButtonUp("Jump"))
+		{
+			jumpMaxTimer = 0;
+			jumpSpeed = 1f;
+			Debug.Log("Hoppas");
+		}
+	}
+
+	private void OnTriggerEnter2D(Collider2D collision)
+	{
+		isJumping = false;
+		Debug.Log("Hoppat");
 	}
 }
