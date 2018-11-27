b829873df267ce8354d9e13b19bf019e38e6d17f
Karatesvennis <petter.svensson.90@gmail.com>
2018-11-27 14:00:49 +0100

    Added movement, player input
    incomplete, need jump script

---
+++ Unity/TheTwilightAdventuresInUpAndDown/Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInput : MonoBehaviour {
+
+    public PlayerMovement move;
+    //jump variable?
+	
+	// Update is called once per frame
+	void Update () {
+        move.Movement(Input.GetAxisRaw("Horizontal"));
+        //jump input + script HERE
+	}
+}
---
+++ Unity/TheTwilightAdventuresInUpAndDown/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public float speed;
+    public Rigidbody2D rigidbody2D;
+	
+	
+	public void Movement (float direction) {
+
+        float x = direction * speed;
+
+        rigidbody2D.velocity = new Vector2(x, rigidbody2D.velocity.y);
+    }
+}
