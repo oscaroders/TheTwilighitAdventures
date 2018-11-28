df7a97c40cb270d243ee394bae510244f2b93591
oscaroders <oscar.oders@gmail.com>
2018-11-28 10:21:51 +0100

    added Gravity invert script

---
+++ Unity/TheTwilightAdventuresInUpAndDown/Assets/Scripts/GravityInvert.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GravityInvert : MonoBehaviour {
+
+    Rigidbody2D rigidbody2D;
+
+	// Use this for initialization
+	void Start () {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+        rigidbody2D.gravityScale *= -1;
+	}
+}
