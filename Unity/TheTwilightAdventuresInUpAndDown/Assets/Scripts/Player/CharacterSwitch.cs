9d3b8ccf73fb64b3e13d7b176550714c7ae9c611
fiolot <sebastian_kba@hotmail.com>
2018-11-28 10:16:40 +0100

    Added Character Switching
    You can now switch between Eve and Dodo. You have to manually put in the player input from Dodo to Eve's CharacterSwitch script.

---
+++ Unity/TheTwilightAdventuresInUpAndDown/Assets/Scripts/Player/CharacterSwitch.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterSwitch : MonoBehaviour
+{
+    public PlayerInput eve, dodo;
+    void Update ()
+    {
+		if(Input.GetKeyDown(KeyCode.K))
+        {
+            eve.move.Movement(0);
+            eve.enabled = !eve.enabled;
+            dodo.move.Movement(0);
+            dodo.enabled = !dodo.enabled;
+        }
+	}
+}
