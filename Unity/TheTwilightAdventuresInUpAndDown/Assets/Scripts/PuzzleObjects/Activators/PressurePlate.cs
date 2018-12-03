947e0e4af5b25f3c5a0b41d663fee34c920f8e06
fiolot <sebastian_kba@hotmail.com>
2018-12-03 14:35:00 +0100

    Update PressurePlate.cs
    It can now have a timer.

--- Unity/TheTwilightAdventuresInUpAndDown/Assets/Scripts/PuzzleObjects/Activators/PressurePlate.cs
+++ Unity/TheTwilightAdventuresInUpAndDown/Assets/Scripts/PuzzleObjects/Activators/PressurePlate.cs
@@ -5,12 +5,25 @@ using UnityEngine;
 public class PressurePlate : MonoBehaviour
 {
     public Activator activator;
+    public bool timerOn;
+    public float timer;
+    private float currentTime;
+    private bool off = true;
     private void OnTriggerEnter2D(Collider2D collision)
     {
         activator.Activate(true);
+        off = false;
     }
     private void OnTriggerExit2D(Collider2D collision)
     {
-        activator.Activate(false);
+        currentTime = 0;
+        off = true;
+    }
+    private void Update()
+    {
+        if (off && (!timerOn || currentTime > timer))
+            activator.Activate(false);
+        if(timerOn)
+            currentTime += Time.deltaTime;
     }
 }
