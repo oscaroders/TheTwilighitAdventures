d9488831d8a52c2852eeaafe0b05537f9994e24e
fiolot <sebastian_kba@hotmail.com>
2018-12-03 14:38:50 +0100

    Added TODO
    TODO Colliders!

--- Unity/TheTwilightAdventuresInUpAndDown/Assets/Scripts/PuzzleObjects/Open.cs
+++ Unity/TheTwilightAdventuresInUpAndDown/Assets/Scripts/PuzzleObjects/Open.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 public class Open : PuzzleAction
 {
+    //TODO Make it work for all colliders in 2D
     private BoxCollider2D colliderBox;
     public bool activate;
     private void Start()
