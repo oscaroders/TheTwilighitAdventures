7b3ec1580b636ab6ba727935a6567b8b1b95489b
fiolot <sebastian_kba@hotmail.com>
2018-12-04 09:47:32 +0100

    Added requirements
    Related Issues:
    - #51

--- Unity/TheTwilightAdventuresInUpAndDown/Assets/Scripts/PuzzleObjects/Activators/PressurePlate.cs
+++ Unity/TheTwilightAdventuresInUpAndDown/Assets/Scripts/PuzzleObjects/Activators/PressurePlate.cs
@@ -1,7 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+[RequireComponent(typeof(Activator))]
+[RequireComponent(typeof(BoxCollider2D))]
 public class PressurePlate : MonoBehaviour
 {
     public Activator activator;
