3d370cf4888b7e71bd0049fce5e00e61bb7caa7c
fiolot <sebastian_kba@hotmail.com>
2018-12-05 14:00:34 +0100

    Added a win and lose condition

---
+++ Unity/TheTwilightAdventuresInUpAndDown/Assets/Scripts/EndOfRoom.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndOfRoom : ActionObject
+{
+    [SerializeField] private bool isLevelEnd;
+    [SerializeField] private string sceneName;
+    public override void OnActivation(bool activated)
+    {
+        if (activated)
+        {
+            if(isLevelEnd)
+            {
+                SceneManager.LoadScene(sceneName);
+            }
+            else
+            {
+                //Pan the camera to the next room
+            }
+        }
+    }
+}
---
+++ Unity/TheTwilightAdventuresInUpAndDown/Assets/Scripts/OutOfBounds.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(Zone))]
+public class OutOfBounds : ActionObject
+{
+    public Transform eve, dodo;
+    public Transform eveCheckpoint, dodoCheckpoint;
+    public override void OnActivation(bool activated)
+    {
+        if (activated)
+        {
+            eve.position = eveCheckpoint.position;
+            dodo.position = dodoCheckpoint.position;
+        }
+    }
+}
