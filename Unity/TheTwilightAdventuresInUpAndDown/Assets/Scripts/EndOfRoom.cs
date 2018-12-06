2ea93cfb6f8558b0881393dbbd598477f4fe258c
Knase23 <Knase23@users.noreply.github.com>
2018-12-06 19:33:37 +0100

    Updated MVP and Edge Colliders When finishing a room.

--- Unity/TheTwilightAdventuresInUpAndDown/Assets/Scripts/EndOfRoom.cs
+++ Unity/TheTwilightAdventuresInUpAndDown/Assets/Scripts/EndOfRoom.cs
@@ -7,6 +7,33 @@ public class EndOfRoom : ActionObject
 {
     [SerializeField] private bool isLevelEnd;
     [SerializeField] private string sceneName;
+
+    private CameraEdgeSnapping upEdgeSnapping;
+    private CameraEdgeSnapping downEdgeSnapping;
+
+    public Transform[] blockers;
+
+    public Transform nextRoomLeft;
+    public Transform nextRoomRight;
+
+    private void Start()
+    {
+        CameraEdgeSnapping[] allEdgeSnapping = FindObjectsOfType<CameraEdgeSnapping>();
+        foreach (CameraEdgeSnapping item in allEdgeSnapping)
+        {
+            if(item.gameObject.name == "Up Camera")
+            {
+                upEdgeSnapping = item;
+            }
+            if (item.gameObject.name == "Down Camera")
+            {
+                downEdgeSnapping = item;
+            }
+        }
+        
+
+    }
+
     public override void OnActivation(bool activated)
     {
         if (activated)
@@ -17,7 +44,19 @@ public class EndOfRoom : ActionObject
             }
             else
             {
+                upEdgeSnapping.leftBound = nextRoomLeft;
+                upEdgeSnapping.rightBound = nextRoomRight;
+
+                downEdgeSnapping.leftBound = nextRoomLeft;
+                downEdgeSnapping.rightBound = nextRoomRight;
                 //Pan the camera to the next room
+                foreach (Transform item in blockers)
+                {
+                    Vector3 tempPos = item.position;
+                    tempPos.x -= 1f;
+                    item.position = tempPos;
+                }
+                this.enabled = false;
             }
         }
     }
