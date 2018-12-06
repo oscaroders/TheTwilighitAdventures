949f7227cd25e609db3af24d8379322d0256e83a
Karatesvennis <petter.svensson.90@gmail.com>
2018-12-06 15:11:31 +0100

    Update Rotate.cs

--- Unity/TheTwilightAdventuresInUpAndDown/Assets/Scripts/Puzzle Pieces/Rotate.cs
+++ Unity/TheTwilightAdventuresInUpAndDown/Assets/Scripts/Puzzle Pieces/Rotate.cs
@@ -6,28 +6,22 @@ public class Rotate : ActionObject
 {
     private Quaternion startRotation;
     public float rotation;
-    //private Animator animata;
     public bool activate;
+
     private void Start()
     {
         startRotation = transform.rotation;
-        //colliderBox = GetComponent<Collider2D>();
-        //animata = GetComponent<Animator>();
     }
     public override void OnActivation(bool activated)
     {
         activate = activated;
         if (activated)
         {
-            transform.rotation = Quaternion.AngleAxis(rotation, Vector3.forward);
-            //colliderBox.enabled = false;
-            //animata.Play("DoorOpen");
+            transform.rotation = Quaternion.AngleAxis(rotation, Vector3.forward);           
         }
         else
         {
             transform.rotation = startRotation;
-            //colliderBox.enabled = true;
-            //animata.Play("DoorClose");
         }
     }
 }
