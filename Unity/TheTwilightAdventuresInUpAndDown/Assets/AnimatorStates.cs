086fec683a1fdf6a25a59e1526f6bef3641b92c7
oscaroders <oscar.oders@gmail.com>
2018-12-06 10:46:11 +0100

    MainMenu

---
+++ Unity/TheTwilightAdventuresInUpAndDown/Assets/AnimatorStates.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class AnimatorStates : MonoBehaviour {
+
+    Animator animator;
+
+    bool mainMenu = true;
+    bool about = false;
+    bool settings = false;
+    bool filters = false;
+    bool controls = false;
+    bool language = false;
+    bool audioMenu = false;
+
+	// Use this for initialization
+	void Start () {
+        animator = GetComponent<Animator>();
+        animator.SetBool("mainMenu", true);
+        animator.SetBool("about", false);
+        animator.SetBool("settings", false);
+        animator.SetBool("filters", false);
+        animator.SetBool("controls", false);
+        animator.SetBool("language", false);
+        animator.SetBool("audio", false);
+    }
+
+    public void AboutAnimation() {
+        about = animator.GetBool("about");
+        mainMenu = animator.GetBool("mainMenu");
+        animator.SetBool("mainMenu", !mainMenu);
+        animator.SetBool("about", !about);
+    }    
+
+    public void SettingsAnimation() {
+        settings = animator.GetBool("settings");
+        mainMenu = animator.GetBool("mainMenu");
+        animator.SetBool("mainMenu", !mainMenu);
+        animator.SetBool("settings", !settings);
+    }
+
+    public void FilterAnimation() {
+        filters = animator.GetBool("filters");
+        settings = animator.GetBool("settings");
+        animator.SetBool("settings", !settings);
+        animator.SetBool("filters", !filters);
+    }
+
+    public void ControlsAnimation() {
+        controls = animator.GetBool("controls");
+        settings = animator.GetBool("settings");
+        animator.SetBool("settings", !settings);
+        animator.SetBool("controls", !controls);
+    }
+
+    public void AudioAnimation() {
+        audioMenu = animator.GetBool("audio");
+        settings = animator.GetBool("settings");
+        animator.SetBool("settings", !settings);
+        animator.SetBool("audio", !audioMenu);
+    }
+
+    public void LanguageAnimation() {
+        language = animator.GetBool("language");
+        settings = animator.GetBool("settings");
+        animator.SetBool("settings", !settings);
+        animator.SetBool("language", !language);
+    }
+
+    public void LoadScene(string scene) {
+        SceneManager.LoadScene(scene);
+    }
+
+    public void ContinueLoad() {
+        //string scene = Get secne and room
+        //SceneManager.LoadScene();
+    }
+
+    public void Quit() {
+        Application.Quit();
+    }
+	
+}
---
+++ Unity/TheTwilightAdventuresInUpAndDown/Assets/MenuCameraController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuCameraController : MonoBehaviour {
+
+    AnimatorStates animatorStates;
+    Animator animator;
+
+	// Use this for initialization
+	void Start () {
+        animatorStates = GetComponent<AnimatorStates>();
+        animator = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetButtonDown("Back")) {
+            if (animator.GetBool("about")) {
+                animatorStates.AboutAnimation();
+            }
+            if (animator.GetBool("settings")) {
+                animatorStates.SettingsAnimation();
+            }
+            if (animator.GetBool("filters")) {
+                animatorStates.FilterAnimation();
+            }
+            if (animator.GetBool("controls")) {
+                animatorStates.ControlsAnimation();
+            }
+            if (animator.GetBool("language")) {
+                animatorStates.LanguageAnimation();
+            }
+            if (animator.GetBool("audio")) {
+                animatorStates.AudioAnimation();
+            }
+            if (animator.GetBool("mainMenu")) {
+                animatorStates.Quit();
+            }
+        }
+	}
+}
