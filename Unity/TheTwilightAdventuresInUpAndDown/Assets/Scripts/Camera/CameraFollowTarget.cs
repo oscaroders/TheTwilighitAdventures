b65fefe71634dc89e2f140e611a6c6f3e680480d
Knase23 <jesper.uddefors.95@gmail.com>
2018-12-07 11:05:07 +0100

    Update CameraFollowTarget.cs
    It actually follows the target

--- Unity/TheTwilightAdventuresInUpAndDown/Assets/Scripts/Camera/CameraFollowTarget.cs
+++ Unity/TheTwilightAdventuresInUpAndDown/Assets/Scripts/Camera/CameraFollowTarget.cs
@@ -6,8 +6,22 @@ public class CameraFollowTarget : MonoBehaviour {
 
     public Transform target;
     public Vector3 offset;
+
+    private void Start()
+    {
+        offset = transform.position + target.position;
+    }
+
     // Update is called once per frame
     void LateUpdate () {
-        transform.position = target.position + offset;
-	}
+
+        Vector3 cameraMovement = target.position - transform.position;
+        //Take 5% of this vector
+        cameraMovement.x = cameraMovement.x * 0.05f;
+        cameraMovement.y *= 0.05f;
+        cameraMovement.z = 0;
+
+        //move that 5% closer
+        transform.Translate(cameraMovement);
+    }
 }
