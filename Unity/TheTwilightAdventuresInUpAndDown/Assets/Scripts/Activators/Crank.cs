2c3ad1366cf2596d357c7f27e892b66b1117c529
Flarne <ulrik.bergstrom@gmail.com>
2018-12-07 15:16:49 +0100

    Made Crank script

---
+++ Unity/TheTwilightAdventuresInUpAndDown/Assets/Scripts/Activators/Crank.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(Activator))]
+
+public class Crank : MonoBehaviour
+{
+	private Activator activator;
+	private int timesPressed;
+	public int requiredPressed;
+
+	// Use this for initialization
+	void Start ()
+	{
+		activator = GetComponent<Activator>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (Input.GetButtonDown("Interact"))
+		{
+			timesPressed++;
+		}
+
+		if (timesPressed == requiredPressed)
+		{
+			activator.stateOfActivator = true;
+		}
+	}
+}
