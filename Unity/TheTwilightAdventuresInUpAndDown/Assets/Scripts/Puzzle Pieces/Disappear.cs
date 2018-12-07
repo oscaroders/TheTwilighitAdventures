2ed609f8b7c36e09c41e5e358d9c5b5a71dcdc19
Karatesvennis <petter.svensson.90@gmail.com>
2018-12-07 10:31:39 +0100

    Created disappear script

---
+++ Unity/TheTwilightAdventuresInUpAndDown/Assets/Scripts/Puzzle Pieces/Disappear.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Disappear : ActionObject
+{
+    private Collider2D colliderBox;
+    private SpriteRenderer spriteRenderer;
+    public bool activate;
+    private void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        colliderBox = GetComponent<Collider2D>();
+    }
+    public override void OnActivation(bool activated)
+    {
+        activate = activated;
+        if (activated)
+        {
+            spriteRenderer.enabled = false;
+            colliderBox.enabled = false;
+        }
+        else
+        {
+            spriteRenderer.enabled = true;
+            colliderBox.enabled = true;
+        }
+    }
+}
