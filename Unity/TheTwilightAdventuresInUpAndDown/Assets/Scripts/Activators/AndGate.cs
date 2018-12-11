62271c404ad10117efb41bef5aac08e4908372e9
Flarne <ulrik.bergstrom@gmail.com>
2018-12-11 15:44:02 +0100

    Added And/Or sccripts

---
+++ Unity/TheTwilightAdventuresInUpAndDown/Assets/Scripts/Activators/AndGate.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(Activator))]
+
+public class AndGate : Activator
+{
+	public Activator[] activators;
+	private bool allActive = false;
+
+	public virtual void OnActivation(bool activated)
+	{
+
+	}
+
+	public void AndGates()
+	{
+		allActive = true;
+		for (int i = 0; i < activators.Length; i++)
+		{
+			if (!activators[i].stateOfActivator)
+			{
+				allActive = false;
+			}
+		}
+		OnActivation(allActive);
+	}
+}
---
+++ Unity/TheTwilightAdventuresInUpAndDown/Assets/Scripts/Activators/OrGate.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Activator))]
+
+public class OrGate : Activator
+{
+	public Activator[] activators;
+
+	public void OrGates()
+	{
+		stateOfActivator = false;
+		for (int i = 0; i < activators.Length; i++)
+		{
+			if (activators[i].stateOfActivator)
+			{
+				stateOfActivator = true;
+				break;
+			}
+		}
+	}
+
+	private void FixedUpdate()
+	{
+		OrGates();
+	}
+}
+
