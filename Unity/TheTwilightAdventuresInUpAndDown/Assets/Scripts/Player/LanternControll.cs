ae2234169a69cbc3dd7e68004b0b338f77c75242
oscaroders <oscar.oders@gmail.com>
2018-12-11 16:03:19 +0100

    LanternLight

---
+++ Unity/TheTwilightAdventuresInUpAndDown/Assets/Scripts/Player/LanternControll.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LanternControll : MonoBehaviour {
+
+    public GameObject lanterLight;
+     // [HideInInspector]
+    public bool hasEveLantern;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Make into a function when we know from where to call it.
+	void Update () {
+
+        if (hasEveLantern) {
+            lanterLight.SetActive(true);
+        } else {
+            lanterLight.SetActive(false);
+        }
+    }
+
+    void SetSizeOfLight(float percentage) {
+        float tmp = percentage / 100;
+        tmp = Mathf.Clamp(tmp, .2f, 1f); 
+
+        lanterLight.transform.localScale = new Vector3(lanterLight.transform.localScale.x * tmp, lanterLight.transform.localScale.y * tmp, lanterLight.transform.localScale.z);
+    }
+}
