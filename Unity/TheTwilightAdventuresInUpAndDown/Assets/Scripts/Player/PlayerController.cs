60e9edf268318da51f0e6705dd97c7ab6cb32027
fiolot <sebastian_kba@hotmail.com>
2018-12-12 14:52:37 +0100

    Added gravityscale for air and ground

--- Unity/TheTwilightAdventuresInUpAndDown/Assets/Scripts/Player/PlayerController.cs
+++ Unity/TheTwilightAdventuresInUpAndDown/Assets/Scripts/Player/PlayerController.cs
@@ -24,6 +24,8 @@ public class PlayerController : MonoBehaviour {
     [Range(0, 10)] [SerializeField] private float fallMultiplier = 5.5f;
     [Range(0, 10)] [SerializeField] private float lowJumpMultiplier = 5f;
     const float groundedRadius = .15f;
+    [Range(0, 10)] [SerializeField] private float groundGravityScale;
+    [Range(0, 10)] [SerializeField] private float airGravityScale;
     [Space]
 
     [SerializeField] private bool airControl = false;
@@ -37,29 +39,37 @@ public class PlayerController : MonoBehaviour {
 
     private UnityEvent OnLandEvent;
 
+    private float characterMult;
+
     private void Awake() {
         rigidBody2D = GetComponent<Rigidbody2D>();
 
         if (OnLandEvent == null)
             OnLandEvent = new UnityEvent();
+        if (rigidBody2D.gravityScale < 0)
+            characterMult = -1;
+        else
+            characterMult = 1;
     }
 
     private void FixedUpdate() {
 
         bool wasGrounded = grounded;
         grounded = false;
-
+        
         // The player is grounded if a circlecast to the groundcheck position hits anything designated as ground
         Collider2D[] colliders = Physics2D.OverlapCircleAll(groundCheck.position, groundedRadius, whatIsGround);
         for (int i = 0; i < colliders.Length; i++) {
             if (colliders[i].gameObject != gameObject) {
                 grounded = true;
+                rigidBody2D.gravityScale = groundGravityScale * characterMult;
                 if (!wasGrounded)
                     OnLandEvent.Invoke();
             }
         }
         if (!grounded)
         {
+            rigidBody2D.gravityScale = airGravityScale * characterMult;
             if ((rigidBody2D.gravityScale > 0 && rigidBody2D.velocity.y < 0) || (rigidBody2D.gravityScale < 0 && rigidBody2D.velocity.y > 0))
             {
                 rigidBody2D.velocity += Vector2.up * Mathf.Clamp(rigidBody2D.gravityScale, -1, 1) * Physics2D.gravity.y * (fallMultiplier - 1) * Time.deltaTime;
