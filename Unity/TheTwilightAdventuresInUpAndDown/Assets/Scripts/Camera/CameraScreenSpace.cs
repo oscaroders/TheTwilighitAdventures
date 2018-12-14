72ef04c45a0d0b3ca435b9fdbbb13b80f9bcbd87
Knase23 <jesper.uddefors.95@gmail.com>
2018-12-14 10:31:50 +0100

    A small transition when changing character. And some minor camera changes
    A small transition
    Camera view more under the character

--- Unity/TheTwilightAdventuresInUpAndDown/Assets/Scripts/Camera/CameraScreenSpace.cs
+++ Unity/TheTwilightAdventuresInUpAndDown/Assets/Scripts/Camera/CameraScreenSpace.cs
@@ -8,42 +8,84 @@ public class CameraScreenSpace : MonoBehaviour {
     public Camera mainCamera;
     public Camera eveCamera;
     public Camera dodoCamera;
-    [Range(0,1)]public float focusProcentage = 0.7f;
+    [Range(0,1)]
+    public float focusProcentage = 0.7f;
 
     public float defaultValueOfOrthographicSize;
 
     private CharacterSwitch characterSwitch;
 
-    float eveCameraProcent;
-    float dodoCameraProcent;
+    internal float eveCameraProcentTotal;
+    internal float dodoCameraProcentTotal;
+    internal float eveCameraCurrent;
+    internal float dodoCameraCurrent;
+
+    [Range(0.010f,0.005f)]
+    public float procentSpeed = 0.01f;
 
     private void Start()
     {
         defaultValueOfOrthographicSize = mainCamera.orthographicSize;
         characterSwitch = GetComponent<CharacterSwitch>();
         Focus("Eve");
+        eveCameraCurrent = eveCameraProcentTotal;
+        dodoCameraCurrent = dodoCameraProcentTotal;
     }
     void LateUpdate () {
 
         if(characterSwitch.isEve)
         {
             Focus("Eve");
+            
+            
         } else
         {
             Focus("Dodo");
         }
 
+
+        if (characterSwitch.isEve && eveCameraCurrent < eveCameraProcentTotal)
+        {
+            eveCameraCurrent += procentSpeed;
+        }
+        else if (!characterSwitch.isEve && eveCameraCurrent > eveCameraProcentTotal)
+        {
+            eveCameraCurrent -= procentSpeed;
+        }
+        else
+        {
+            eveCameraCurrent = eveCameraProcentTotal;
+        }
+
+       
+       if (!characterSwitch.isEve && dodoCameraCurrent < dodoCameraProcentTotal)
+       {
+            dodoCameraCurrent += procentSpeed;
+       }
+       else if(characterSwitch.isEve && dodoCameraCurrent > dodoCameraProcentTotal)
+       {
+            dodoCameraCurrent -= procentSpeed;
+       }
+       else
+       {
+            dodoCameraCurrent = dodoCameraProcentTotal;
+       }
+       
+
         mainCamera.orthographicSize = defaultValueOfOrthographicSize;
 
-        eveCamera.orthographicSize = defaultValueOfOrthographicSize * eveCameraProcent;
-        dodoCamera.orthographicSize = defaultValueOfOrthographicSize * dodoCameraProcent;
+        eveCamera.orthographicSize = defaultValueOfOrthographicSize * eveCameraCurrent;
+        dodoCamera.orthographicSize = defaultValueOfOrthographicSize * dodoCameraCurrent;
+
+       
+
 
         Rect temp = eveCamera.rect;
-        temp.y = -eveCameraProcent + 1;
+        temp.y = -eveCameraCurrent + 1;
         eveCamera.rect = temp;
 
         temp = dodoCamera.rect;
-        temp.y = dodoCameraProcent - 1;
+        temp.y = dodoCameraCurrent - 1;
         dodoCamera.rect = temp;
     }
 
@@ -51,13 +93,13 @@ public class CameraScreenSpace : MonoBehaviour {
     {
         if(name == "Eve")
         {
-            eveCameraProcent = focusProcentage;
-            dodoCameraProcent = 1 - focusProcentage;
+            eveCameraProcentTotal = focusProcentage;
+            dodoCameraProcentTotal = 1 - focusProcentage;
         }
         else if(name == "Dodo")
         {
-            dodoCameraProcent = focusProcentage;
-            eveCameraProcent = 1 - focusProcentage;
+            dodoCameraProcentTotal = focusProcentage;
+            eveCameraProcentTotal = 1 - focusProcentage;
         }
 
     }
