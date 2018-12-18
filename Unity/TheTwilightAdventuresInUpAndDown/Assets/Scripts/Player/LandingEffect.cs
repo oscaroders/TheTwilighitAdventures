e04e234216ed5d61c88d39caadbd8452f2ccf38e
Karatesvennis <petter.svensson.90@gmail.com>
2018-12-18 15:57:31 +0100

    Added particle systems for walking and landing
    In particles - prefabs. OBS! Not Finished visually with particle effects.

---
+++ Unity/TheTwilightAdventuresInUpAndDown/Assets/Scripts/Player/LandingEffect.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LandingEffect : MonoBehaviour {
+
+    private ParticleSystem particleSystem;
+    private PlayerController playerController;
+    private bool hasLanded;
+
+	// Use this for initialization
+	void Start () {
+        ParticleSystem[] particleSystems = GetComponentsInChildren<ParticleSystem>();
+        foreach (ParticleSystem particle in particleSystems)
+        {
+            if (particle.tag == "Landing")
+            {
+                particleSystem = particle;
+            }
+        }
+        playerController = GetComponent<PlayerController>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (playerController.grounded && hasLanded == false)
+        {
+            particleSystem.Play();
+            hasLanded = true;
+        }
+        if (playerController.grounded == false)
+        {
+            hasLanded = false;
+        }
+	}
+}
---
+++ Unity/TheTwilightAdventuresInUpAndDown/Assets/Scripts/Player/WalkingEffect.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WalkingEffect : MonoBehaviour {
+
+    private Rigidbody2D rigidbody2D;
+    private ParticleSystem particleSystem;
+    private PlayerController playerController;
+
+    // Use this for initialization
+    void Start () {
+        ParticleSystem[] particleSystems = GetComponentsInChildren<ParticleSystem>();
+        foreach (ParticleSystem particle in particleSystems)
+        {
+            if (particle.tag == "Moving")
+            {
+                particleSystem = particle;
+            }
+        }
+        playerController = GetComponent<PlayerController>();
+        rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (playerController.grounded && rigidbody2D.velocity.magnitude > 1)
+        {
+            particleSystem.Play();
+        }
+        else
+        {
+            particleSystem.Stop();
+        }
+	}
+}
