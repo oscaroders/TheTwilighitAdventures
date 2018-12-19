e3dabedbb53e3d313c340d057eaba9329824eefb
oscaroders <oscar.oders@gmail.com>
2018-12-19 10:36:21 +0100

    Flash when die

--- Unity/TheTwilightAdventuresInUpAndDown/Assets/Scripts/OutOfBounds.cs
+++ Unity/TheTwilightAdventuresInUpAndDown/Assets/Scripts/OutOfBounds.cs
@@ -4,14 +4,42 @@ using UnityEngine;
 [RequireComponent(typeof(Zone))]
 public class OutOfBounds : ActionObject
 {
+    public Animator animator;
+
+    public PlayerInput input;
     public Transform eve, dodo;
     public Transform eveCheckpoint, dodoCheckpoint;
     public override void OnActivation(bool activated)
     {
         if (activated)
         {
-            eve.position = eveCheckpoint.position;
-            dodo.position = dodoCheckpoint.position;
+            FadeIn();
+            MoveToCheckpoint();
+            FreezePlayer();
+             
         }
+    }
+
+    void MoveToCheckpoint() {
+        eve.position = eveCheckpoint.position;
+        dodo.position = dodoCheckpoint.position;
+    }
+
+    void FadeIn() {
+        animator.SetTrigger("FadeIn");
+        Invoke("RetrunToStartInAnimator", 1f);
+    }
+
+    void RetrunToStartInAnimator() {
+        animator.SetTrigger("Start");
+        Invoke("UnFreezePlayer", 0.1f);
+    }
+
+    public void FreezePlayer() {
+        input.enabled = false;
+    }
+
+    public void UnFreezePlayer() {
+        input.enabled = true;
     }
 }
