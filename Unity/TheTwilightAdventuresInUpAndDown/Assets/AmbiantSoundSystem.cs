3b75c0041c7be4dbcc3c21b6c23d50ec79e93ce4
Knase23 <jesper.uddefors.95@gmail.com>
2019-01-03 16:04:42 +0100

    Prepare for AmbiantSound Fade in and out

---
+++ Unity/TheTwilightAdventuresInUpAndDown/Assets/AmbiantSoundSystem.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AmbiantSoundSystem : MonoBehaviour {
+
+    public AudioSource eveAmbientSound;
+    public AudioSource dodoAmbientSound;
+    private float eveAmbientDefualtVolume;
+    private float dodoAmbientDefualtVolume;
+    private PlayerInput pi;
+	// Use this for initialization
+	void Start () {
+        pi = FindObjectOfType<PlayerInput>();
+        eveAmbientDefualtVolume = eveAmbientSound.volume;
+        dodoAmbientDefualtVolume = dodoAmbientSound.volume;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        BackGroundMusic();
+	}
+    public void BackGroundMusic()
+    {
+        if (pi.IsEveInFocus && !eveAmbientSound.isPlaying)
+        {
+            dodoAmbientSound.Pause();
+            eveAmbientSound.Play();
+        }
+        else if (!pi.IsEveInFocus && !dodoAmbientSound.isPlaying)
+        {
+            eveAmbientSound.Pause();
+            dodoAmbientSound.Play();
+        }
+    }
+}
