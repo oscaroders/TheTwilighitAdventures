a8d235858d65b9574a8867fa2463569bdbad873c
fiolot <sebastian_kba@hotmail.com>
2019-01-04 15:57:32 +0100

    Added rudimentary animation for Eve
    Jogging and jumping

---
+++ Unity/TheTwilightAdventuresInUpAndDown/Assets/Scripts/Player/JustTestingAnimation.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JustTestingAnimation : MonoBehaviour
+{
+    private Animator animatorPlayer;
+    private PlayerController playerController;
+    private SpriteRenderer eve;
+	void Start ()
+    {
+        animatorPlayer = GetComponent<Animator>();
+        playerController = GetComponent<PlayerController>();
+        eve = GetComponentInChildren<SpriteRenderer>();
+	}
+	void Update ()
+    {
+        if (playerController.velocity.x > 1 || playerController.velocity.x < -1)
+        {
+            animatorPlayer.SetBool("moving", true);
+        }
+        else
+            animatorPlayer.SetBool("moving", false);
+
+        animatorPlayer.SetFloat("jumpVelocity", playerController.velocity.y);
+	}
+}
