299ccee865dba7e597c2bc17bcd87db2552857c2
oscaroders <oscar.oders@gmail.com>
2019-01-07 15:28:32 +0100

    moving platform work

---
+++ Unity/TheTwilightAdventuresInUpAndDown/Assets/ActivatePlatform.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivatePlatform : ActionObject {
+
+    private bool activated;
+    PlatformController platformController;
+
+    // Use this for initialization
+    void Start () {
+        platformController = GetComponent<PlatformController>();
+        platformController.enabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (activated) {
+            platformController.enabled = true;
+        } else if (!activated) {
+            platformController.enabled = false;
+        }
+    }
+
+    public override void OnActivation(bool activated) {
+
+        this.activated = activated;
+    }
+}
