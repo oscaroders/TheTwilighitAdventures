80664f934f96d86d815180982126c3c3b20fdf18
oscaroders <oscar.oders@gmail.com>
2019-01-08 16:31:53 +0100

    textpromt and cloud

---
+++ Unity/TheTwilightAdventuresInUpAndDown/Assets/PromptFlip.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PromptFlip : MonoBehaviour {
+
+    public PlayerInput playerInput;
+    public PlayerController player;
+    public Transform trans;
+    bool firstTimeHere = true;
+
+    Text text;
+
+
+    // Use this for initialization
+    void Start() {
+        text = GetComponent<Text>();
+
+        text.enabled = false;
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+        if (playerInput.currentRoomId == 2 && playerInput.IsEveInFocus) {
+            float distance = Vector3.Distance(trans.position, player.transform.position);
+
+            if (distance < 2.5 && firstTimeHere) {
+                text.enabled = true;
+                if (Input.GetButton("FlipWorld")) {
+                    firstTimeHere = false;
+                }
+
+            } else {
+                text.enabled = false;
+
+            }
+        }
+    }
+}
---
+++ Unity/TheTwilightAdventuresInUpAndDown/Assets/PromptSwitch.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PromptSwitch : MonoBehaviour {
+
+    public PlayerInput playerInput;
+
+    bool showText = true;
+    Text text;
+
+
+    // Use this for initialization
+    void Start() {
+        text = GetComponent<Text>();
+
+        text.enabled = false;
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+        if (playerInput.currentRoomId == 2) {
+
+            if (showText && !playerInput.IsEveInFocus) {
+                text.enabled = true;
+                if (Input.GetButtonDown("CharacterSwitch")) {
+                    showText = false;
+                }
+
+            } else {
+                text.enabled = false;
+
+            }
+        }
+    }
+}
